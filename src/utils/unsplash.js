5a7ff0e77ed5d0dc8fb32c91962609f888c4a706
Igor Pysarneko <pysarenko_ia@groupbwt.com>
2021-02-08 00:57:56 +0200

    fix

--- src/utils/unsplash.js
+++ src/utils/unsplash.js
@@ -3,7 +3,8 @@ import Unsplash from "unsplash-js";
 const unsplash = new Unsplash({
   accessKey: process.env.VUE_APP_UNSPLASH_API_KEY,
   secret: process.env.VUE_APP_UNSPLASH_API_SECRET,
-  callbackUrl: "https://pysarenko-ia.groupbwt.dev/",
+  callbackUrl:
+    "https://602070315610f9b2254b19e0--upbeat-wozniak-486305.netlify.app/",
 });
 
 export default unsplash;
