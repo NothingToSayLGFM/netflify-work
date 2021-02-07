31b2c38b9f6ae47a3241a05eefd54c45cb4a0cba
Igor Pysarneko <pysarenko_ia@groupbwt.com>
2021-02-08 01:08:55 +0200

    change keys

--- src/utils/unsplash.js
+++ src/utils/unsplash.js
@@ -3,8 +3,7 @@ import Unsplash from "unsplash-js";
 const unsplash = new Unsplash({
   accessKey: process.env.VUE_APP_UNSPLASH_API_KEY,
   secret: process.env.VUE_APP_UNSPLASH_API_SECRET,
-  callbackUrl:
-    "https://602070315610f9b2254b19e0--upbeat-wozniak-486305.netlify.app/",
+  callbackUrl: "https://upbeat-wozniak-486305.netlify.app/",
 });
 
 export default unsplash;
